3921a6b0599d6c4def6cdd6ef2c0ce5cb7572704
Munesh Chowdary <32429098+muneshchowdary@users.noreply.github.com>
2021-01-19 23:16:52 +0530

    Update main.tf
    adding vpc

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
 terraform {
-    required_version= ">=0.11.0"
+  required_version = ">=0.11.0"
 }
 
-provider aws {
-    version = "~> 2.0"
-    region = "us-west-2"
+provider "aws" {
+  version    = "~> 2.0"
+  region     = "us-west-2"
+  access_key = "AKIAZLXVCJEQ53XJX4FT"
+  secret_key = "F7bf1ymSmzFS9leKJ/T/lXP+ApFLIwmSvmaNgB4P"
 }
 
 resource "aws_instance" "sample" {
-    
-    ami = "ami-00d1b5cc1e5341681"
-    type = "t2.micro"
+
+  ami           = "ami-00d1b5cc1e5341681"
+  instance_type = "t2.micro"
+
 }
 
+resource "aws_vpc" "sample_vpc" {
+  cidr_block = "10.0.0.0/16"
 
+}
