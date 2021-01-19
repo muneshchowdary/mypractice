25fd5630569276ff03a90749c6470d6a7ad0141e
Munesh Chowdary <32429098+muneshchowdary@users.noreply.github.com>
2021-01-20 00:39:59 +0530

    Add files via upload
    adding variables file

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = ">=0.11.0"
+    required_version= ">=0.11.0"
 }
 
-provider "aws" {
-  version    = "~> 2.0"
-  region     = "us-west-2"
-  access_key = "AKIAZLXVCJEQ53XJX4FT"
-  secret_key = "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"
+provider aws {
+    version = "~> 2.0"
+    region = "us-west-2"
+    access_key = var.access_key
+    secret_key = var.sec_key
 }
 
 resource "aws_instance" "sample" {
-
-  ami           = "ami-00d1b5cc1e5341681"
-  instance_type = "t2.micro"
-
+    
+    ami = "ami-00d1b5cc1e5341681"
+    type = "t2.micro"
 }
 
-resource "aws_vpc" "sample_vpc" {
+resource "aws_vpc" "sample" {
   cidr_block = "10.0.0.0/16"
-
+  
 }
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {
+default = "AKIAZLXVCJEQ3TK4HGE2"
+  
+}
+
+variable "sec_key" {
+  default = "29UtMtmrYWDMEY6Ll02HQ7b2yoTEHxD5enFC9r3G"
+}
